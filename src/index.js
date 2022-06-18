9c3f3578859da55e112032dc152fd6a0f160ac90
xiumubai <1547702880@qq.com>
2022-06-18 21:23:33 +0800

    feat: 单页应用的路由与持久缓存优化

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+/*
+ * @Author: 朽木白
+ * @Date: 2022-06-18 17:27:44
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-06-18 21:20:40
+ * @Description:
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
